0ce6c96c7cced1bcfadae65a2c2b8bcd097ab47a
jiaabid <jia.abid98@gmail.com>
2020-03-13 10:00:22 +0500

    fixing auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next) => {
     try {
 
         // const token = req.header('Authorization').replace("Bearer ", "")
-        const toke = req.query.token
+        const token = req.query.token
         const decoded = jwt.verify(token, process.env.JWTKEY)
         console.log(decoded, token)
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
